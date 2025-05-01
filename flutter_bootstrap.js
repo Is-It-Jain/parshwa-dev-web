be473263c47363ed4f1afb6008d449c4e3eed416
Parshwa <parshwa282011@gmail.com>
2025-05-01 13:34:37 +0530

    Fixing spelling erorrs

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "744049043"
+    serviceWorkerVersion: "2399576070"
   }
 });
